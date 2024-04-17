ed17b40d75e1022a28af388a0b66357d22fb574a
Vonraj <radhruinn@gmail.com>
2024-04-17 16:31:57 +0300

    change

--- scripts/volume_slide.sh
+++ scripts/volume_slide.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 #
-#
-# Check if volumeicon is running
-if pgrep -x "volumeicon" > /dev/null
-then
-    # If running, kill volumeicon
-    pkill volumeicon
-else
-    # If not running, start volumeicon
     volumeicon &
-fi
+
